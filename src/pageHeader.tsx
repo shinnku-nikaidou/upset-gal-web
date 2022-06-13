ca641f6cfbaddec78c8e4225bf7bc5d09e231539
shinnku <shinnkunikaidou@gmail.com>
2022-06-14 00:21:31 +0100

    change title

--- src/pageHeader.tsx
+++ src/pageHeader.tsx
@@ -4,7 +4,7 @@ export const GalPageHead = () =>
   <PageHeader
     className="site-page-header"
     title={
-      <div>失落的小站</div>
+      <div>失落小站</div>
     }
     subTitle="欢迎来到 galgame 分享站点"
   ></PageHeader>
