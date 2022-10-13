cd19d1a557419fb51d4b4876c8142af09a035bf7
shinnku <shinnkunikaidou@gmail.com>
2022-10-13 17:23:46 +0800

    add search box

---
+++ components/leftDrawer.tsx
@@ -0,0 +1,38 @@
+import {
+  useDisclosure,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerHeader,
+  DrawerBody,
+  Button,
+  Drawer,
+} from '@chakra-ui/react'
+import { HamburgerIcon } from '@chakra-ui/icons'
+import React from 'react'
+
+const LeftDrawer = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
+  return (
+    <>
+      <Button onClick={onOpen} bg="whiteAlpha.50">
+        <HamburgerIcon />
+      </Button>
+      <Drawer placement={'left'} onClose={onClose} isOpen={isOpen}>
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerHeader borderBottomWidth='1px'>shinnku website</DrawerHeader>
+          <DrawerBody>
+            <p>Some strange shit...</p>
+            <p>Some contents...</p>
+            <p>Some contents...</p>
+            <p>Some contents...</p>
+            <p>Some contents...</p>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+    </>
+  )
+}
+
+export default LeftDrawer
