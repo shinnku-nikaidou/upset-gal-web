45ca670817b13065e5e9e62c52cd98cc02d095e3
shinnku <shinnkunikaidou@gmail.com>
2023-04-08 21:00:28 +0800

    update恰饭

--- components/download/Readme/Feedback.tsx
+++ components/download/Readme/Feedback.tsx
@@ -14,6 +14,20 @@ export const Feedback = ({ lang }: { lang: string }) => (
         </Link>
         <span>{t('Feedback-last', lang)}</span>
       </Text>
+      <Text>
+        <span>
+          {' '}
+          鉴于每天都有人在tg群询问梯子事宜（顺便恰饭），这里推荐一个站长（可爱美少女）自用机场{' '}
+        </span>
+        <Link
+          target='_blank'
+          rel='noreferrer'
+          href='https://xixiyun.top/#/register?code=wqT1x5ma'
+        >
+          西西云
+        </Link>
+        <span> 。已经和机场主达成合作协议了。 </span>
+      </Text>
     </Space>
   </Panel>
 )
