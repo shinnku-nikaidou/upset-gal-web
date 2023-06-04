6f1d5523a421048f538b698da673fc0b3b698b3c
shinnku <shinnkunikaidou@gmail.com>
2023-06-04 09:41:30 -0400

    add cache

--- pages/api/download/_fetchAccessToken.ts
+++ pages/api/download/_fetchAccessToken.ts
@@ -1,22 +1,20 @@
-import { OauthDrive, AccessToken } from "../../../data/downloadtype";
-import fetchFormData from "./_fetchFormData";
-
+import { OauthDrive, AccessToken } from '../../../data/downloadtype'
+import fetchFormData from './_fetchFormData'
 
 export default async function fetchAccessToken(
-  oauth_drive: OauthDrive
+  oauth_drive: OauthDrive,
 ): Promise<string> {
-  const refreshToken = oauth_drive.refreshToken;
+  const refreshToken = oauth_drive.refreshToken
 
-  const url = oauth_drive["oauthUrl"] + "token";
+  const url = oauth_drive['oauthUrl'] + 'token'
   const data: AccessToken = {
     client_id: oauth_drive.clientId,
     client_secret: oauth_drive.clientSecret,
-    grant_type: "refresh_token",
-    requested_token_use: "on_behalf_of",
+    grant_type: 'refresh_token',
+    requested_token_use: 'on_behalf_of',
     refresh_token: refreshToken,
-  };
-  const result = await fetchFormData(url, data);
+  }
+  const result = await fetchFormData(url, data)
 
-  return result.access_token;
+  return result.access_token
 }
-
--- pages/api/download/_query.ts
+++ pages/api/download/_query.ts
@@ -1,19 +1,19 @@
-import { OauthDrive } from "../../../data/downloadtype";
-import getContentWithHeaders from "../../../utils/getContentWithHeaders";
-import fetchAccessToken from "./_fetchAccessToken";
+import { OauthDrive } from '../../../data/downloadtype'
+import getContentWithHeaders from '../../../utils/getContentWithHeaders'
+import fetchAccessToken from './_fetchAccessToken'
 
 export default async function query_one(
   oauth_drive: OauthDrive,
   path: string,
-  params?: string
+  params?: string,
 ): Promise<Headers> {
-  const accessToken = await fetchAccessToken(oauth_drive);
+  const accessToken = await fetchAccessToken(oauth_drive)
 
-  let uri = oauth_drive.apiUrl + encodeURI(path);
-  if (params) uri = uri + "?" + params;
-  console.log(uri);
+  let uri = oauth_drive.apiUrl + encodeURI(path)
+  if (params) uri = uri + '?' + params
+  console.log(uri)
   const body = await getContentWithHeaders(uri, {
-    Authorization: "Bearer " + accessToken,
-  } as unknown as Headers);
-  return body;
+    Authorization: 'Bearer ' + accessToken,
+  } as unknown as Headers)
+  return body
 }
