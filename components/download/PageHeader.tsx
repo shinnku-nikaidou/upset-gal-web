ae419fd645290dd8e532640b8085c7d98d5b6c08
AsukaMinato <i@asukaminato.eu.org>
2023-06-16 04:37:47 +0900

    fix by https://github.com/ant-design/ant-design/issues/16037

--- components/download/PageHeader.tsx
+++ components/download/PageHeader.tsx
@@ -8,6 +8,18 @@ export const GalPageHeader = (props: { lang: string }) => {
       className='header'
       style={{ position: 'sticky', top: 0, zIndex: 1, width: '100%' }}
     >
+      <style
+        id="holderStyle"
+        dangerouslySetInnerHTML={{
+          __html: `
+            /* https://github.com/ant-design/ant-design/issues/16037#issuecomment-483140458 */
+            /* Not only antd, but also any other style if you want to use ssr. */
+            *, *::before, *::after {
+              transition: none!important;
+            }
+          `
+        }}
+      />
       <div
         style={{
           float: 'left',
