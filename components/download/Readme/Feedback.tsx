4e032cc700889d96df76768da970a53c47139b09
shinnku <shinnkunikaidou@gmail.com>
2023-06-21 22:40:17 -0400

    delete xixi

--- components/download/Readme/Feedback.tsx
+++ components/download/Readme/Feedback.tsx
@@ -35,15 +35,6 @@ export const Feedback = ({ lang }: { lang: string }) => (
         </Link>
         <span>
           （节点名字全部都是galgame，都推荐下载玩一遍哦）。
-          还有另外一个性价比极高的机场
-          <Link
-            target='_blank'
-            rel='noreferrer'
-            href='https://xixiyun.top/#/register?code=wqT1x5ma'
-          >
-            西西云
-          </Link>
-          ，9元200gb/19元800gb,推荐都注册使用喵,全都可以无限免费体验。
           使用机场的订阅链接请在这里下载开源的clash客户端，导入订阅链接(clash下载:
           <Link
             target='_blank'
