37fd800fbec6aab3077631318eb683926f9e87b4
shinnku <nikaidou@shinnku.com>
2023-10-04 13:26:23 +0800

    update legacy

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import t from '@lang'
-import Main from '@components/download/Main'
+import Main from '@/components/legacy/Main'
 import { addDefaultProp } from '@utils/addDefaultProp'
 import Script from 'next/script'
 
