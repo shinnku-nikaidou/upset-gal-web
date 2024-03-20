98f058f88f59ca810994fc281097e9e82d8889c7
shinnku <shinnkunikaidou@gmail.com>
2024-03-20 14:45:04 +0800

    save

--- components/legacy/Readme/Intro.tsx
+++ components/legacy/Readme/Intro.tsx
@@ -1,15 +1,27 @@
-import { Collapse, Space, Typography } from 'antd/lib'
 import t from '@lang'
 import Link from 'next/link'
-
-const { Panel } = Collapse
-const { Text } = Typography
+import {
+  AccordionButton,
+  AccordionIcon,
+  AccordionItem,
+  AccordionPanel,
+  Box,
+  Text,
+} from '@chakra-ui/react'
 
 export const Intro = ({ lang }: { lang: string }) => (
-  <Panel header={t('Intro', lang)} key='1'>
-    <Space direction='vertical'>
+  <AccordionItem>
+    <h2>
+      <AccordionButton>
+        <Box as='span' flex='1' textAlign='left'>
+          {t('Intro', lang)}
+        </Box>
+        <AccordionIcon />
+      </AccordionButton>
+    </h2>
+    <AccordionPanel pb={4}>
       <Text>{t('Intro1', lang)}</Text>
       <Text>{t('Intro2', lang)}</Text>
-    </Space>
-  </Panel>
+    </AccordionPanel>
+  </AccordionItem>
 )
--- components/legacy/Readme/index.tsx
+++ components/legacy/Readme/index.tsx
@@ -4,6 +4,14 @@ import { CaretRightOutlined } from '@ant-design/icons'
 import { Intro } from './Intro'
 import { Feedback } from './Feedback'
 import { Notice } from './Notice'
+import {
+  Accordion,
+  AccordionButton,
+  AccordionIcon,
+  AccordionItem,
+  AccordionPanel,
+  Box,
+} from '@chakra-ui/react'
 
 export const Readme = ({
   lang,
@@ -13,6 +21,42 @@ export const Readme = ({
   isMobile: boolean
 }) => (
   <>
+    <Accordion defaultIndex={isMobile ? [2] : [1, 2, 3]} allowMultiple>
+      {Intro({ lang })}
+      <AccordionItem>
+        <h2>
+          <AccordionButton>
+            <Box as='span' flex='1' textAlign='left'>
+              Section 1 title
+            </Box>
+            <AccordionIcon />
+          </AccordionButton>
+        </h2>
+        <AccordionPanel pb={4}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </AccordionPanel>
+      </AccordionItem>
+
+      <AccordionItem>
+        <h2>
+          <AccordionButton>
+            <Box as='span' flex='1' textAlign='left'>
+              Section 2 title
+            </Box>
+            <AccordionIcon />
+          </AccordionButton>
+        </h2>
+        <AccordionPanel pb={4}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </AccordionPanel>
+      </AccordionItem>
+    </Accordion>
     <Collapse
       bordered={false}
       defaultActiveKey={isMobile ? ['2'] : ['1', '2', '3']}
@@ -20,7 +64,6 @@ export const Readme = ({
         <CaretRightOutlined rotate={isActive ? 90 : 0} />
       )}
     >
-      {Intro({ lang })}
       {Feedback({ lang })}
       {Notice({ lang })}
     </Collapse>
