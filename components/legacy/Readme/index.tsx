08d1d76cc69554338884bb532f2c106ba6a7f69e
shinnku <nikaidou@shinnku.com>
2024-05-31 22:48:29 -0600

    update Accordion

--- components/legacy/Readme/index.tsx
+++ components/legacy/Readme/index.tsx
@@ -4,7 +4,11 @@ import { Notice } from './Notice'
 import { Accordion } from '@nextui-org/react'
 
 export const Readme = ({ lang }: { lang: string; isMobile: boolean }) => (
-  <Accordion variant='shadow' defaultExpandedKeys={['1', '2', '3']}>
+  <Accordion
+    variant='shadow'
+    selectionMode='multiple'
+    defaultExpandedKeys={['1', '2', '3']}
+  >
     {Intro({ lang })}
     {Feedback({ lang })}
     {Notice({ lang })}
