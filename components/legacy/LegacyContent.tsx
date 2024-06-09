a2524c086bebc657d41320dffb233fb88d248074
shinnku <nikaidou@shinnku.com>
2024-06-09 08:07:09 -0600

    add qq-2

--- components/legacy/LegacyContent.tsx
+++ components/legacy/LegacyContent.tsx
@@ -74,13 +74,13 @@ const LegacyContent = (props: DefaultInfoProp) => {
                     alt='Woman listing to music'
                     className='object-cover'
                     height={200}
-                    src='/assets/qq.jpg'
+                    src='/assets/qq-2.jpg'
                     width={200}
                   />
                   <CardFooter className='justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10'>
                     <Link
                       className='text-tiny text-white/80'
-                      href='https://qm.qq.com/q/5VSFae9HTW'
+                      href='https://qm.qq.com/q/cWzTi1eoTK'
                       target='_blank'
                     >
                       入群链接
