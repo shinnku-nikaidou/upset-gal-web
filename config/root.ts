068410e5b96a7099ae99a586f694667e69a148e5
shinnku <nikaidou@shinnku.com>
2025-03-01 15:46:15 +0800

    add some debug api

--- config/root.ts
+++ config/root.ts
@@ -34,3 +34,9 @@ function generateTree(fileList: FileInfo[]): any {
 }
 
 export const shinnku_tree = generateTree(shinnku_bucket_files_json)
+export const galgame0_tree = generateTree(galgame0_bucket_files_json)
+
+export const tree = {
+  shinnku: shinnku_tree,
+  galgame0: galgame0_tree['合集系列']['浮士德galgame游戏合集'],
+}
--- pages/api/data/tree.ts
+++ pages/api/data/tree.ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-import { galgame0_tree } from '@/config/root'
+import { tree } from '@/config/root'
 
 export default function handler(_req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json(galgame0_tree)
+  res.status(200).json(tree)
 }
