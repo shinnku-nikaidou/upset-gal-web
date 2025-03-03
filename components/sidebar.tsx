82c111b37883d637df06444b7561926049716fda
KUN1007 <yuyuyukunkunkun@gmail.com>
2025-03-04 04:10:43 +0800

    feat: file page sidebar highlight

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -2,10 +2,13 @@
 
 import { Button } from '@heroui/button'
 import { Link } from '@heroui/link'
+import { usePathname } from 'next/navigation'
 
 import { IndexListForSlog } from '@/config/indexList'
 
 export const Sidebar = () => {
+  const pathname = usePathname()
+
   return (
     <div className='flex flex-col gap-4'>
       {IndexListForSlog.map((item, index) => (
@@ -14,8 +17,9 @@ export const Sidebar = () => {
             isIconOnly
             as={Link}
             className='flex md:hidden'
+            color={pathname === item.link ? 'primary' : 'default'}
             href={item.link}
-            variant='bordered'
+            variant={pathname === item.link ? 'solid' : 'bordered'}
           >
             {item.body}
           </Button>
@@ -23,8 +27,9 @@ export const Sidebar = () => {
             fullWidth
             as={Link}
             className='justify-start hidden md:flex'
+            color={pathname === item.link ? 'primary' : 'default'}
             href={item.link}
-            variant='bordered'
+            variant={pathname === item.link ? 'solid' : 'bordered'}
           >
             {item.body}
             <span>{item.title}</span>
