d512f5afce446bd7b6e09ef6e21578eefe60ddc7
KUN1007 <yuyuyukunkunkun@gmail.com>
2025-03-04 02:35:12 +0800

    feat: add loading indicator

--- app/providers.tsx
+++ app/providers.tsx
@@ -6,6 +6,7 @@ import * as React from 'react'
 import { HeroUIProvider } from '@heroui/system'
 import { useRouter } from 'next/navigation'
 import { ThemeProvider as NextThemesProvider } from 'next-themes'
+import { AppProgressProvider as ProgressProvider } from '@bprogress/next'
 
 export interface ProvidersProps {
   children: React.ReactNode
@@ -18,7 +19,14 @@ export function Providers({ children, themeProps }: ProvidersProps) {
   return (
     <HeroUIProvider navigate={router.push}>
       <NextThemesProvider attribute='class' {...themeProps}>
-        {children}
+        <ProgressProvider
+          shallowRouting
+          color='#006FEE'
+          height='4px'
+          options={{ showSpinner: false }}
+        >
+          {children}
+        </ProgressProvider>
       </NextThemesProvider>
     </HeroUIProvider>
   )
